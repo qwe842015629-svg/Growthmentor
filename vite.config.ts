ebb0522d360c88b1d256feb2afa52580fa4086aa
Roes_Growth <qwe842015629@gmail.com>
2025-11-26 07:23:35 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,22 @@
 import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '.'),
+    }
+  },
+  // 👇👇👇 关键修改在这里 👇👇👇
+  build: {
+    target: "esnext" // 允许使用最新的 JS 特性 (Top-level await)
+  },
+  // ☝️☝️☝️ 关键修改在这里 ☝️☝️☝️
+  
+  server: {
+    port: 3000,
+    host: '0.0.0.0',
+  }
 });
